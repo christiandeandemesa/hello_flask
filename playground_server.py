7a777bbbfed463bb9381acd2151b6916ac7f3ef0
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-09-30 17:58:35 -0700

    9/30/21 commit

---
+++ playground_server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/play')
+def level_one():
+    return render_template("playground.html", num=3, color="lightblue")
+
+@app.route('/play/<int:num>')
+def level_two(num):
+    return render_template("playground.html", num=num, color="lightblue")
+
+@app.route('/play/<int:num>/<string:color>')
+def level_three(num, color):
+    return render_template("playground.html", num=num, color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)
